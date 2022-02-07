8e8da165ee3b888b40b4b7b21544cfe63b121510
github_mysia14 <mysiasanta@gmail.com>
2022-02-07 11:57:38 -0700

    added bmi calculator functions

--- imc/src/App.js
+++ imc/src/App.js
@@ -1,15 +1,42 @@
+import {useState} from "react"
 import "./app.css";
 export default function App () {
+  const [weight, setWeight] = useState ("");
+  const [height, setHeight] = useState ("");
+  
+  function calculateBMI() {
+    const altura = height / 100;
+    const bmi = weight / (altura * altura);
+
+    if(bmi < 18.6){
+      alert ("Underweight Range! Your BMI is " + bmi.toFixed(2))
+    }
+    else if ( bmi >= 18.6 && bmi <24.9){
+      alert ("Healthy Weight Range! Your BMI is " + bmi.toFixed(2))
+    }
+    else if (bmi > 24.9 && bmi < 34.9){
+      alert ("Overweight Range! Your BMI is " + bmi.toFixed(2))
+    }
+    else if (bmi > 34.9){
+      alert ("Obese Range! Your BMI is " + bmi.toFixed(2))
+    }
+
+  }
+
+ 
+
   return(
     <div className="app">
       <h1>BMI Calculator</h1>
       <span>Let's Calculate your BMI</span>
 
       <div className="area-input">
-        <input type="text" placeholder="Your wheight (Kg)" />
-        <input type="text" placeholder="Your height (cm)" />
+        <input type="text" placeholder="Your weight (Kg)" 
+         value = {weight} onChange = { (e) => setWeight (e.target.value)}/>
+        <input type="text" placeholder="Your height (cm)"
+         value = {height} onChange = { (e) => setHeight (e.target.value)} />
 
-        <button>
+        <button onClick = {calculateBMI}>
           Calculate
         </button>
       </div>
